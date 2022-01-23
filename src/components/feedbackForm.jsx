33044580455984204b8a65bf2ae0609bb4e21839
Bz-Carter <lebz2016@gmail.com>
2022-01-23 01:58:51 +0000

    feedback interface created .

---
+++ src/components/feedbackForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Card from "./shared/card";
+import Button from "./shared/button";
+import RatingSelect from "./ratingSelect";
+
+function FeedbackForm({ handleAdd }) {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState("");
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisable(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be least 10 characters");
+      setBtnDisable(true);
+    } else {
+      setMessage(null);
+      setBtnDisable(false);
+    }
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+
+      setText('');
+    }
+  };
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisable}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "lorem ipsum sit amet consectetur adipisicing elit. consequmtur vel vitae comodi alias voluptatem est voluptatum ipsa",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "lorem ipsum sit amet consectetur adipisicing elit. consequmtur vel vitae comodi alias voluptatem est voluptatum ipsa",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "lorem ipsum sit amet consectetur adipisicing elit. consequmtur vel vitae comodi alias voluptatem est voluptatum ipsa",
+  },
+];
+
+export default FeedbackData;
